acd1132d36dc17a055e9936646f131414abd12e2
Hamide Bay <hamide.bay@baycom.ch>
2020-12-16 23:07:00 +0100

    kellePaca.js is added

---
+++ kellePaca.js
@@ -0,0 +1,19 @@
+
+//ciftlikteki hayvanlar
+let hayvanlar = ["inek", "tavuk", "koyun", "tavuk", "domuz", "tavuk", "koyun", "tavuk"];
+let toplamBacak = 0;
+
+//hayvanlarin cinsine göre bacak sayisini toplayan döngü
+for (let i = 0; i < hayvanlar.length; i++) {
+  let hayvanCinsi = hayvanlar[i];
+  if (
+    hayvanCinsi == "inek" ||
+    hayvanCinsi == "koyun" ||
+    hayvanCinsi == "domuz"
+  ) {
+    toplamBacak += 4;
+  } else {
+    toplamBacak += 2;
+  }
+ }
+console.log(toplamBacak);
